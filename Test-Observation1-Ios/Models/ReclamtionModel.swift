d07b6032d6e0641d5bc93bf9edc925b5782c801c
kooli mootez <mootez.kooli@esprit.tn>
2025-01-14 18:16:17 +0100

    feat:Declarations-Api

---
+++ Test-Observation1-Ios/Models/ReclamtionModel.swift
@@ -0,0 +1,13 @@
+//
+//  ReclamtionModel.swift
+//  Test-Observation1-Ios
+//
+//  Created by KOOLI MOOTEZ on 14/01/2025.
+//
+
+struct Post1: Codable, Identifiable {
+    let id: Int
+    let title: String
+    let body: String
+}
+
